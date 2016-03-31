8d9d4c29b27fe2fd5869c0dfaa8c069799d84b4b
Axel Schelfhout <axelschelfhout@gmail.com>
2016-03-31 13:44:06 +0200

    Runs based on last row. This is wrong, it needs to run based on last matrix (whole set)

--- gameoflife.py
+++ gameoflife.py
@@ -2,54 +2,125 @@ from numpy import genfromtxt
 
 data = genfromtxt('game-of-life-input1.csv', delimiter=',')
 
+
 def gameoflife(data):
     dead = 0
     alive = 1
 
-    data_length = len(data)
-    data_counter = 0
+    amount_of_rows = len(data)
+    row_counter = 0
 
     for row in data:
 
-        if data_counter < data_length:
+        row = list(map(int, row))  # Parse row to int
+
+        if row_counter < amount_of_rows:
 
             row_length = len(row)
-            row_counter = 0
+            cell_counter = 0
             printer = ''
 
-            for content in row:
+            for cell in row:
 
-                if row_counter < row_length:
+                if cell_counter < row_length:
                     alive_neighbours = 0
+                    dead_neighbours = 0
 
                     # Up
+                    if row_counter == 0:
+                        dead_neighbours += 1
+                    elif data[row_counter-1][cell_counter] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
 
                     # Up Right
+                    if row_counter == 0:
+                        dead_neighbours += 1
+                    elif cell_counter == row_length - 1:
+                        dead_neighbours += 1
+                    elif data[row_counter - 1][cell_counter + 1] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
 
                     # Right
-
+                    if cell_counter == row_length - 1:
+                        dead_neighbours += 1
+                    elif row[cell_counter + 1] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
+                        
                     # Down Right
+                    if row_counter == amount_of_rows - 1:
+                        dead_neighbours += 1
+                    elif cell_counter == row_length - 1:
+                        dead_neighbours += 1
+                    elif data[row_counter + 1][cell_counter + 1] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
 
                     # Down
+                    if row_counter == amount_of_rows - 1:
+                        dead_neighbours += 1
+                    elif data[row_counter + 1][cell_counter] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
 
                     # Down Left
+                    if row_counter == amount_of_rows - 1:
+                        dead_neighbours += 1
+                    elif cell_counter == 0:
+                        dead_neighbours += 1
+                    elif data[row_counter + 1][cell_counter - 1] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
 
                     # Left
+                    if cell_counter == 0:
+                        dead_neighbours += 1
+                    elif row[cell_counter - 1] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
 
                     # Up Left
+                    if row_counter == 0:
+                        dead_neighbours += 1
+                    elif cell_counter == 0:
+                        dead_neighbours += 1
+                    elif data[row_counter - 1][cell_counter - 1] == alive:
+                        alive_neighbours += 1
+                    else:
+                        dead_neighbours += 1
+
+                    # Check if the cell should be alive or dead
+                    if cell == alive and 2 >= alive_neighbours <= 3:
+                        cell = alive
+                    elif cell == dead and alive_neighbours == 3:
+                        cell = alive
+                    else:
+                        cell = dead
+
+                    # print(data[row_counter][cell_counter])
+                    data[row_counter][cell_counter] = cell
+                    # print(data[row_counter][cell_counter])
 
                     out = '*'
-                    if int(content) == dead:
+                    if cell == dead:
                         out = '.'
 
                     printer += str(out)
-                    row_counter += 1
+                    cell_counter += 1
             print(printer)
-        data_counter += 1
-
-    return
+        row_counter += 1
+    return data
 
 i = 0
 while i < 10:
-    gameoflife(data)
+    data = gameoflife(data)
     i += 1
