fa052883c5438bb96f041d4825c47f00bdeb4631
Axel Schelfhout <axelschelfhout@gmail.com>
2016-03-31 14:45:37 +0200

    Optimized

--- gameoflife.py
+++ gameoflife.py
@@ -3,6 +3,7 @@ from numpy import genfromtxt
 data = genfromtxt('game-of-life-input1.csv', delimiter=',')
 #data = genfromtxt('example.csv', delimiter=',')
 
+
 def gameoflife(data):
 
     dead = 0
@@ -12,7 +13,8 @@ def gameoflife(data):
     row_counter = 0
 
     # Create new matrix to put the new cell states in
-    new_data = [[0 for col in range(amount_of_rows)] for row in range(len(data[0]))]
+    # This is created with the amount of rows and columns
+    new_data = [[0 for cols in range(amount_of_rows)] for rows in range(len(data[0]))]
 
     for row in data:
         row = list(map(int, row))  # Parse row to int
@@ -27,79 +29,54 @@ def gameoflife(data):
 
                 if cell_counter < row_length:
                     alive_neighbours = 0
-                    dead_neighbours = 0
 
                     # Up
                     if row_counter == 0:
-                        dead_neighbours += 1
+                        pass
                     elif data[row_counter-1][cell_counter] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
 
                     # Up Right
-                    if row_counter == 0:
-                        dead_neighbours += 1
-                    elif cell_counter == row_length - 1:
-                        dead_neighbours += 1
+                    if row_counter == 0 or cell_counter == row_length - 1:
+                        pass
                     elif data[row_counter - 1][cell_counter + 1] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
 
                     # Right
                     if cell_counter == row_length - 1:
-                        dead_neighbours += 1
+                        pass
                     elif row[cell_counter + 1] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
                         
                     # Down Right
-                    if row_counter == amount_of_rows - 1:
-                        dead_neighbours += 1
-                    elif cell_counter == row_length - 1:
-                        dead_neighbours += 1
+                    if row_counter == amount_of_rows - 1 or cell_counter == row_length - 1:
+                        pass
                     elif data[row_counter + 1][cell_counter + 1] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
 
                     # Down
                     if row_counter == amount_of_rows - 1:
-                        dead_neighbours += 1
+                        pass
                     elif data[row_counter + 1][cell_counter] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
 
                     # Down Left
-                    if row_counter == amount_of_rows - 1:
-                        dead_neighbours += 1
-                    elif cell_counter == 0:
-                        dead_neighbours += 1
+                    if row_counter == amount_of_rows - 1 or cell_counter == 0:
+                        pass
                     elif data[row_counter + 1][cell_counter - 1] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
 
                     # Left
                     if cell_counter == 0:
-                        dead_neighbours += 1
+                        pass
                     elif row[cell_counter - 1] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
 
                     # Up Left
-                    if row_counter == 0:
-                        dead_neighbours += 1
-                    elif cell_counter == 0:
-                        dead_neighbours += 1
+                    if row_counter == 0 or cell_counter == 0:
+                        pass
                     elif data[row_counter - 1][cell_counter - 1] == alive:
                         alive_neighbours += 1
-                    else:
-                        dead_neighbours += 1
 
                     # Check if the cell should be alive or dead
                     if cell == alive and alive_neighbours == 2 or alive_neighbours == 3:
